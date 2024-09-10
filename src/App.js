5ed50b842d237e9151a3f7abc0a582dc82a94f2a
Michal Sobanski <michaelsobanski2015@gmail.com>
2024-09-10 16:29:19 +0000

    New - added first dinamic Value

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+const readDescription = ["Fundamental", "Crucial", "Core"];
+
+const genRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
+
 const Header = () => {
+  const description = readDescription[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
