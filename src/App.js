db27ea0f09c9cb3150c6dd32ce6e464fb347fd5b
Michal Sobanski <michaelsobanski2015@gmail.com>
2024-09-12 14:26:22 +0000

    New - Outputting List Data Dynamically

--- src/App.js
+++ src/App.js
@@ -41,15 +41,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            {/* Alternative prop syntax - if keys are same as prop names*/}
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
